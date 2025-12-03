23bc5f5c426930b95313fba9c2870f7656909bc9
Glêisson de Oliveira Bezerra <oliveirabezerraeu@gmail.com>
2025-12-03 19:55:31 -0300

    atualizações da Aula

---
+++ src/assets/App.tsx
@@ -0,0 +1,15 @@
+export function App() {
+  console.log('oi');
+  return (
+    <div>
+      <h1>Olá, mundo!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+        veritatis voluptatibus molestias quisquam error nesciunt praesentium
+        aperiam dolore pariatur autem quas fugiat, perferendis repudiandae,
+        exercitationem quae! Soluta qui deserunt amet.
+      </p>
+    </div>
+  );
+}
+
